7ddc2e27b530860544478d6ae9005b16de85f812
OBaca <78699182+OBaca@users.noreply.github.com>
2024-11-09 11:06:38 +0200

    fixed type issues

--- server/src/database.js
+++ server/src/database.js
@@ -2,23 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-// Load environment variables from .env file
 // Specify the path to the .env file inside the 'src' folder
 dotenv_1.default.config({ path: path_1.default.resolve(__dirname, '../src/trip.env') });
-// Debugging: Check if the environment variables are loaded
-console.log('Database user:', process.env.DB_USER);
-console.log('Database password length:', (_a = process.env.DB_PASSWORD) === null || _a === void 0 ? void 0 : _a.length);
-console.log('Database password:', process.env.DB_PASSWORD);
 const pool = mysql2_1.default.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT),
 });
 // Test the connection
