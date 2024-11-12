c232fd5d39c4b6ac92ded458d43e63603bb9acbd
OBaca <78699182+OBaca@users.noreply.github.com>
2024-11-12 20:44:48 +0200

    Backend (#9)
    * more install dependencies
    
    * fixed install readme
    
    * added config for database
    
    * added config for database#2
    
    * fixed type issues
    
    * add env file
    
    * added opentripmap and wiki api
    
    ---------
    
    Co-authored-by: shayrab <rabinovich162@gmail.com>

--- server/src/routes/app.ts
+++ server/src/routes/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router as tripRoutes } from './tripRoutes';
 import { router as userRoutes } from './userRoutes';
+import { router as opentripmapRoutes } from './opentripmapRoutes'; // Correct path to your opentripmapRoutes file
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 // Use the routes
 app.use('/trips', tripRoutes);
 app.use('/users', userRoutes);
+app.use('/activities', opentripmapRoutes);
 
 // Start the server
 app.listen(PORT, () => {
