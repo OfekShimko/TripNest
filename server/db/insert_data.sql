dc066993d43500c385f1e84d10787366a5928d09
shayrab <rabinovich162@gmail.com>
2024-11-20 23:33:56 +0200

    change the tables in sql as required

--- server/db/insert_data.sql
+++ server/db/insert_data.sql
@@ -1,6 +1,1 @@
-USE trip_nest;
-
-INSERT INTO trips (destination, date, description)
-VALUES 
-('Paris', '2024-12-01', 'Trip to Paris for sightseeing'),
-('London', '2025-01-15', 'Business trip to London');
+SELECT * FROM Activity;
