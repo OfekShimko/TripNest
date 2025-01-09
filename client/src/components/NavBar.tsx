78dc5e2ad79cf792c6917c226771a76385fb42e4
Yana Silutin <yana.silutin@gmail.com>
2025-01-09 12:52:18 +0200

    remove profile from user menu

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -60,13 +60,6 @@ const Navbar = () => {
                     className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none"
                   >
                     <MenuItem
-                      as="a"
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100"
-                    >
-                      Your Profile
-                    </MenuItem>
-                    <MenuItem
                       as="button"
                       onClick={handleSignOut}
                       className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100"
